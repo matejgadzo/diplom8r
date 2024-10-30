566ceea4abc1337845968c7248eb9c67ca3a67d7
matejgadzo <matej.gadzo5@gmail.com>
2024-10-30 21:22:10 +0100

    frontend init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="header">
+        <div className="hero">
+          <h1 className="heroTitle">Diplom8tor</h1>
+        </div>
+        <ul className="menu">
+          <li className="menuItem active">Home</li>
+          <a href="#" className="menuLink">
+            <li className="menuItem">Templates</li>
+          </a>
+          <li className="menuItem">Account</li>
+        </ul>
+      </div>
+      <div className="content">
+        <div className="container">
+          <h1 className="title">Upload a document you want to sign</h1>
+          <div className="uploadArea">
+            <input type="file" />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+      <p className="footerText"> &#169; 2024 CM8</p>
+      </div>
     </div>
   );
 }
---
+++ src/Templates/templates.tsx
@@ -0,0 +1,10 @@
+import "/src/App.scss"
+function Templates() {
+    return (
+        <div className="templates">
+            <h1>Templates</h1>
+        </div>
+    );
+}
+
+export default Templates;
